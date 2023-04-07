c400199639f97e7c5cca66884730270e999c9b49
Lahwin <lahwin.l123@gmail.com>
2023-04-07 15:48:11 +0700

    change App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,11 @@ import { useState } from 'react'
 import './App.css'
 
 import React from 'react';
-import Dictionary from './components/Dictionary';
 import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div >
-      {/* <Dictionary /> */}
-
       <SearchBar />
     </div>
   )
